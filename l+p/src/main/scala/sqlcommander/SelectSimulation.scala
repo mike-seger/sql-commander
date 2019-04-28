356a1bdb8a7589506e2b6c7375bfca2abbcac195
mike-seger <mss@unknown.com>
2019-04-28 06:44:50 +0200

    full initialization by gatling

--- l+p/src/main/scala/sqlcommander/SelectSimulation.scala
+++ l+p/src/main/scala/sqlcommander/SelectSimulation.scala
@@ -16,9 +16,39 @@ class SelectSimulation extends Simulation {
     .userAgentHeader("Java/1.8.0_131")
     .contentTypeHeader(HttpHeaderValues.TextXml)
 
+  val dropCounter = new java.util.concurrent.atomic.AtomicInteger(0)
+  val insertCounter = new java.util.concurrent.atomic.AtomicInteger(0)
+
   val scn = scenario("Select Simulation")
+    .repeat(1) {
+      pause(50 milliseconds)
+      .exec(session => session.set("dropCounter", dropCounter.getAndIncrement))
+      .doIf(session => session("dropCounter").as[Integer] < 1) {
+        exec(http("drop create")
+        .get("/runscript")
+        .queryParam("resourceUrl", "classpath:/static/test/create-message.sql")
+        .check(status.is(200))
+      )}
+//      .exec(session => {
+//        println(session("counter").as[String])
+//        session }
+//      )
+    }
+    .rendezVous(nUsers)
+    .repeat(1000) {
+      pause(20 milliseconds)
+      .exec(session => session.set("insertCounter", insertCounter.getAndIncrement))
+      .doIf(session => session("insertCounter").as[Integer] < 1000) {
+        exec(http("insert messages")
+        .post("/update")
+        .header(HttpHeaderNames.ContentType, "text/plain")
+        .body(StringBody("insert into message(context_id_, text_) values('uuid', 'Message uuid')"))
+        .check(status.is(200))
+      )}
+    }
+    .rendezVous(nUsers)
     .repeat(100) {
-      pause(300 milliseconds)
+      pause(500 milliseconds)
       .exec(http("select messages")
         .post("/select")
         .header(HttpHeaderNames.ContentType, "text/plain")
@@ -28,7 +58,7 @@ class SelectSimulation extends Simulation {
     }
 
   setUp(
-    scn.inject(
-      atOnceUsers(nUsers)
+    scn.inject(rampUsers(nUsers) during (30 seconds)
+     // .atOnceUsers(nUsers)
     ).protocols(httpConf))
 }
