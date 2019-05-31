dfc0cd15367cf35bcc8f943333a7c32cb7b76611
Michael Seger <michael@macbookpro>
2019-05-31 02:13:01 +0200

    test data

--- src/main/resources/static/test/populate.sql
+++ src/main/resources/static/test/populate.sql
@@ -5,13 +5,107 @@ create table person (
     firstName varchar(256), 
     lastName varchar(256), 
     address varchar(256),
+    zip integer,
     primary key(id)
 );
 
-insert into person(firstName, lastName, address)
-    values('first 1', 'last 1', 'address 1');
-insert into person(firstName, lastName, address)
-    values('first 2', 'last 2', 'address 2');
-insert into person(firstName, lastName, address)
-    values('first 3', 'last 3', 'address 3');
-
+insert into person(firstName, lastName, address, zip) values('first 1', 'last 1', 'address 1', 1);
+insert into person(firstName, lastName, address, zip) values('first 2', 'last 2', 'address 2', 2);
+insert into person(firstName, lastName, address, zip) values('first 3', 'last 3', 'address 3', 3);
+insert into person(firstName, lastName, address, zip) values('first 4', 'last 4', 'address 4', 4);
+insert into person(firstName, lastName, address, zip) values('first 5', 'last 5', 'address 5', 5);
+insert into person(firstName, lastName, address, zip) values('first 6', 'last 6', 'address 6', 6);
+insert into person(firstName, lastName, address, zip) values('first 7', 'last 7', 'address 7', 7);
+insert into person(firstName, lastName, address, zip) values('first 8', 'last 8', 'address 8', 8);
+insert into person(firstName, lastName, address, zip) values('first 9', 'last 9', 'address 9', 9);
+insert into person(firstName, lastName, address, zip) values('first 10', 'last 10', 'address 10', 10);
+insert into person(firstName, lastName, address, zip) values('first 11', 'last 11', 'address 11', 11);
+insert into person(firstName, lastName, address, zip) values('first 12', 'last 12', 'address 12', 12);
+insert into person(firstName, lastName, address, zip) values('first 13', 'last 13', 'address 13', 13);
+insert into person(firstName, lastName, address, zip) values('first 14', 'last 14', 'address 14', 14);
+insert into person(firstName, lastName, address, zip) values('first 15', 'last 15', 'address 15', 15);
+insert into person(firstName, lastName, address, zip) values('first 16', 'last 16', 'address 16', 16);
+insert into person(firstName, lastName, address, zip) values('first 17', 'last 17', 'address 17', 17);
+insert into person(firstName, lastName, address, zip) values('first 18', 'last 18', 'address 18', 18);
+insert into person(firstName, lastName, address, zip) values('first 19', 'last 19', 'address 19', 19);
+insert into person(firstName, lastName, address, zip) values('first 20', 'last 20', 'address 20', 20);
+insert into person(firstName, lastName, address, zip) values('first 21', 'last 21', 'address 21', 21);
+insert into person(firstName, lastName, address, zip) values('first 22', 'last 22', 'address 22', 22);
+insert into person(firstName, lastName, address, zip) values('first 23', 'last 23', 'address 23', 23);
+insert into person(firstName, lastName, address, zip) values('first 24', 'last 24', 'address 24', 24);
+insert into person(firstName, lastName, address, zip) values('first 25', 'last 25', 'address 25', 25);
+insert into person(firstName, lastName, address, zip) values('first 26', 'last 26', 'address 26', 26);
+insert into person(firstName, lastName, address, zip) values('first 27', 'last 27', 'address 27', 27);
+insert into person(firstName, lastName, address, zip) values('first 28', 'last 28', 'address 28', 28);
+insert into person(firstName, lastName, address, zip) values('first 29', 'last 29', 'address 29', 29);
+insert into person(firstName, lastName, address, zip) values('first 30', 'last 30', 'address 30', 30);
+insert into person(firstName, lastName, address, zip) values('first 31', 'last 31', 'address 31', 31);
+insert into person(firstName, lastName, address, zip) values('first 32', 'last 32', 'address 32', 32);
+insert into person(firstName, lastName, address, zip) values('first 33', 'last 33', 'address 33', 33);
+insert into person(firstName, lastName, address, zip) values('first 34', 'last 34', 'address 34', 34);
+insert into person(firstName, lastName, address, zip) values('first 35', 'last 35', 'address 35', 35);
+insert into person(firstName, lastName, address, zip) values('first 36', 'last 36', 'address 36', 36);
+insert into person(firstName, lastName, address, zip) values('first 37', 'last 37', 'address 37', 37);
+insert into person(firstName, lastName, address, zip) values('first 38', 'last 38', 'address 38', 38);
+insert into person(firstName, lastName, address, zip) values('first 39', 'last 39', 'address 39', 39);
+insert into person(firstName, lastName, address, zip) values('first 40', 'last 40', 'address 40', 40);
+insert into person(firstName, lastName, address, zip) values('first 41', 'last 41', 'address 41', 41);
+insert into person(firstName, lastName, address, zip) values('first 42', 'last 42', 'address 42', 42);
+insert into person(firstName, lastName, address, zip) values('first 43', 'last 43', 'address 43', 43);
+insert into person(firstName, lastName, address, zip) values('first 44', 'last 44', 'address 44', 44);
+insert into person(firstName, lastName, address, zip) values('first 45', 'last 45', 'address 45', 45);
+insert into person(firstName, lastName, address, zip) values('first 46', 'last 46', 'address 46', 46);
+insert into person(firstName, lastName, address, zip) values('first 47', 'last 47', 'address 47', 47);
+insert into person(firstName, lastName, address, zip) values('first 48', 'last 48', 'address 48', 48);
+insert into person(firstName, lastName, address, zip) values('first 49', 'last 49', 'address 49', 49);
+insert into person(firstName, lastName, address, zip) values('first 50', 'last 50', 'address 50', 50);
+insert into person(firstName, lastName, address, zip) values('first 51', 'last 51', 'address 51', 51);
+insert into person(firstName, lastName, address, zip) values('first 52', 'last 52', 'address 52', 52);
+insert into person(firstName, lastName, address, zip) values('first 53', 'last 53', 'address 53', 53);
+insert into person(firstName, lastName, address, zip) values('first 54', 'last 54', 'address 54', 54);
+insert into person(firstName, lastName, address, zip) values('first 55', 'last 55', 'address 55', 55);
+insert into person(firstName, lastName, address, zip) values('first 56', 'last 56', 'address 56', 56);
+insert into person(firstName, lastName, address, zip) values('first 57', 'last 57', 'address 57', 57);
+insert into person(firstName, lastName, address, zip) values('first 58', 'last 58', 'address 58', 58);
+insert into person(firstName, lastName, address, zip) values('first 59', 'last 59', 'address 59', 59);
+insert into person(firstName, lastName, address, zip) values('first 60', 'last 60', 'address 60', 60);
+insert into person(firstName, lastName, address, zip) values('first 61', 'last 61', 'address 61', 61);
+insert into person(firstName, lastName, address, zip) values('first 62', 'last 62', 'address 62', 62);
+insert into person(firstName, lastName, address, zip) values('first 63', 'last 63', 'address 63', 63);
+insert into person(firstName, lastName, address, zip) values('first 64', 'last 64', 'address 64', 64);
+insert into person(firstName, lastName, address, zip) values('first 65', 'last 65', 'address 65', 65);
+insert into person(firstName, lastName, address, zip) values('first 66', 'last 66', 'address 66', 66);
+insert into person(firstName, lastName, address, zip) values('first 67', 'last 67', 'address 67', 67);
+insert into person(firstName, lastName, address, zip) values('first 68', 'last 68', 'address 68', 68);
+insert into person(firstName, lastName, address, zip) values('first 69', 'last 69', 'address 69', 69);
+insert into person(firstName, lastName, address, zip) values('first 70', 'last 70', 'address 70', 70);
+insert into person(firstName, lastName, address, zip) values('first 71', 'last 71', 'address 71', 71);
+insert into person(firstName, lastName, address, zip) values('first 72', 'last 72', 'address 72', 72);
+insert into person(firstName, lastName, address, zip) values('first 73', 'last 73', 'address 73', 73);
+insert into person(firstName, lastName, address, zip) values('first 74', 'last 74', 'address 74', 74);
+insert into person(firstName, lastName, address, zip) values('first 75', 'last 75', 'address 75', 75);
+insert into person(firstName, lastName, address, zip) values('first 76', 'last 76', 'address 76', 76);
+insert into person(firstName, lastName, address, zip) values('first 77', 'last 77', 'address 77', 77);
+insert into person(firstName, lastName, address, zip) values('first 78', 'last 78', 'address 78', 78);
+insert into person(firstName, lastName, address, zip) values('first 79', 'last 79', 'address 79', 79);
+insert into person(firstName, lastName, address, zip) values('first 80', 'last 80', 'address 80', 80);
+insert into person(firstName, lastName, address, zip) values('first 81', 'last 81', 'address 81', 81);
+insert into person(firstName, lastName, address, zip) values('first 82', 'last 82', 'address 82', 82);
+insert into person(firstName, lastName, address, zip) values('first 83', 'last 83', 'address 83', 83);
+insert into person(firstName, lastName, address, zip) values('first 84', 'last 84', 'address 84', 84);
+insert into person(firstName, lastName, address, zip) values('first 85', 'last 85', 'address 85', 85);
+insert into person(firstName, lastName, address, zip) values('first 86', 'last 86', 'address 86', 86);
+insert into person(firstName, lastName, address, zip) values('first 87', 'last 87', 'address 87', 87);
+insert into person(firstName, lastName, address, zip) values('first 88', 'last 88', 'address 88', 88);
+insert into person(firstName, lastName, address, zip) values('first 89', 'last 89', 'address 89', 89);
+insert into person(firstName, lastName, address, zip) values('first 90', 'last 90', 'address 90', 90);
+insert into person(firstName, lastName, address, zip) values('first 91', 'last 91', 'address 91', 91);
+insert into person(firstName, lastName, address, zip) values('first 92', 'last 92', 'address 92', 92);
+insert into person(firstName, lastName, address, zip) values('first 93', 'last 93', 'address 93', 93);
+insert into person(firstName, lastName, address, zip) values('first 94', 'last 94', 'address 94', 94);
+insert into person(firstName, lastName, address, zip) values('first 95', 'last 95', 'address 95', 95);
+insert into person(firstName, lastName, address, zip) values('first 96', 'last 96', 'address 96', 96);
+insert into person(firstName, lastName, address, zip) values('first 97', 'last 97', 'address 97', 97);
+insert into person(firstName, lastName, address, zip) values('first 98', 'last 98', 'address 98', 98);
+insert into person(firstName, lastName, address, zip) values('first 99', 'last 99', 'address 99', 99);
+insert into person(firstName, lastName, address, zip) values('first 100', 'last 100', 'address 100', 100);
